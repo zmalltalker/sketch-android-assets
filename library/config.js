df4f19c0b99193910fe0c1ff3dec8ec414ca073a
cobanion <cobanion@google.com>
2014-05-02 19:37:13 -0400

    Updated to work with Sketch 3 and added config file so you can manually adjust naming conventions

---
+++ library/config.js
@@ -0,0 +1,22 @@
+configVars = {
+    
+    //Include a prefix? [yes/no]
+    includePrefix: "yes", 
+    
+	// By default, it will prompt you to enter a prefix (examples: launcher, menu, stat_notify, tab, dialog). 
+	// Un-comment the next line to set a prefix manually instead: 
+	// prefixName: "prefix",
+	
+    
+    // Do you want to include a suffix? [yes/no]
+    includeSuffix: "yes",
+    
+    // The default suffix is set to the page name (examples: normal, pressed, focused, disabled, selected). 
+    // Un-comment the next line to set a custom one manually instead:
+	// suffixName: "suffix",
+	
+	
+	// Include the asset width at the end of the file name? [yes/no]
+	size: "no"
+	
+}
