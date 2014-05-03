618d06f76b56961460c0e571d23721e2af8622e8
cobanion <cobanion@google.com>
2014-05-02 20:17:24 -0400

    Updated config file

--- library/config.js
+++ library/config.js
@@ -1,18 +1,21 @@
 configVars = {
     
-    //Include a prefix? [yes/no]
+    // File name options: ic_{prefix}_{slice name}_{suffix}_{size}[@2x].png 
+    // Examples: ic_btn_send_pressed_24.png, ic_action_delete.png, ic_star@2x.png
+    
+    // Include a prefix? [yes/no]
     includePrefix: "yes", 
     
-	// By default, it will prompt you to enter a prefix (examples: launcher, menu, stat_notify, tab, dialog). 
-	// Un-comment the next line to set a prefix manually instead: 
+	// By default, you'll be prompted to enter a prefix (examples: launcher, menu, stat_notify, tab, dialog)
+	// Un-comment the next line to set a custom prefix instead: 
 	// prefixName: "prefix",
 	
     
     // Do you want to include a suffix? [yes/no]
     includeSuffix: "yes",
     
-    // The default suffix is set to the page name (examples: normal, pressed, focused, disabled, selected). 
-    // Un-comment the next line to set a custom one manually instead:
+    // The default suffix is set to the page name (examples: normal, pressed, focused, disabled, selected)
+    // Un-comment the next line to set a custom suffix instead:
 	// suffixName: "suffix",
 	
 	
