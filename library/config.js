a0ac0229aaf549dff533597f582bc6e963b21971
cobanion <cobanion@google.com>
2014-05-04 16:41:37 -0400

    Updated for compatibility with Sketch 3.0.2

--- library/config.js
+++ library/config.js
@@ -1,23 +1,23 @@
 configVars = {
     
-    // File name options: ic_{prefix}_{slice name}_{suffix}_{size}[@2x].png 
-    // Examples: ic_btn_send_pressed_24.png, ic_action_delete.png, ic_star@2x.png
-    
-    // Include a prefix? [yes/no]
+    //Include a prefix? [yes/no]
     includePrefix: "yes", 
     
-	// By default, you'll be prompted to enter a prefix (examples: launcher, menu, stat_notify, tab, dialog)
-	// Un-comment the next line to set a custom prefix instead: 
+	// By default, it will prompt you to enter a prefix (examples: launcher, menu, stat_notify, tab, dialog). 
+	// Un-comment the next line to set a prefix manually instead: 
 	// prefixName: "prefix",
 	
-    
+	
     // Do you want to include a suffix? [yes/no]
     includeSuffix: "yes",
     
-    // The default suffix is set to the page name (examples: normal, pressed, focused, disabled, selected)
-    // Un-comment the next line to set a custom suffix instead:
+    // The default suffix is set to the page name (examples: normal, pressed, focused, disabled, selected). 
+    // Un-comment the next line to set a custom one manually instead:
 	// suffixName: "suffix",
 	
+	
+	// This plugin works best if you design @1x, but if you design @2x (or 3x, or 4x) you can change this number here:
+	baseSize: 1,
 	
 	// Include the asset width at the end of the file name? [yes/no]
 	size: "no"
